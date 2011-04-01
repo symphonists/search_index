94a7afc69efd484fec904461731a0e9c10c9df88
Vlad <vlad_micutul@yahoo.com>
2011-04-01 23:43:30 +0800

    Added Romanian translation.

---
+++ lang/lang.ro.php
@@ -0,0 +1,90 @@
+<?php
+
+	$about = array(
+		'name' => '',
+		'author' => array(
+			'name' => 'Vlad Ghita',
+			'email' => 'vlad.ghita@xandergroup.ro',
+			'website' => ''
+		),
+		'release-date' => '2011-04-01'
+	);
+
+	/**
+	 * Search Index
+	 */
+	$dictionary = array(
+
+		'Search Indexes' => 
+		'Indecşi de căutare',
+
+		'Search Index' => 
+		'',
+
+		'Indexes' => 
+		'Indecşi',
+
+		'Included Fields' => 
+		'Câmpuri Incluse',
+
+		'Only selected fields will be indexed.' => 
+		'Doar câmpurile selectate vor fi incluse.',
+
+		'Weighting' => 
+		'Pondere',
+
+		'sections' => 
+		'secţiuni',
+
+		'authors' => 
+		'autori',
+
+		'navigation' => 
+		'navigare',
+
+		'Index Filters' => 
+		'Filtre pentru Indecşi',
+
+		'Only entries that pass these filters will be indexed.' => 
+		'Doar înregistrările care trec de aceste filtre for fi indexate.',
+
+		'Configure how each of your sections are indexed. Choose which field text values to index, which entries to index, and the weighting of the section in search results.' => 
+		'Configuraţi modul în care fiecare secţiune va fi indexată. Alegeţi care câmpuri şi înregistrări să fie indexate, precum şi ponderea secţiunii în rezultatele căutării.',
+
+		'None Found.' => 
+		'Nimic Găsit.',
+
+		'Waiting to re-index...' => 
+		'Aştept pentru reindexare...',
+
+		'entry' => 
+		'înregistrare',
+
+		'entries' => 
+		'înregistrări',
+
+		'No index' => 
+		'Niciun index',
+
+		'Logs' => 
+		'Logs',
+
+		'Synonyms' => 
+		'Sinonime',
+
+		'Replacement word' => 
+		'Cuvânt pentru înlocuire',
+
+		'Matching synonyms will be replaced with this word.' => 
+		'Sinonimele care se potrivesc vor fi înlocuite cu acest cuvânt.',
+
+		'Word' => 
+		'Cuvânt',
+
+		'These words will be replaced with the word above. Separate multiple words with commas.' => 
+		'Aceste cuvinte vor fi înlocuite cu cuvântul de mai sus. Separaţi variantele multiple prin virgulă.',
+
+		'Configure synonym expansion, so that common misspellings or variations of phrase can be normalised to a single phrase.' => 
+		'Configuraţi extinderea sinonimelor, astfel încât greşelile comune de ortografie sau variaţiile de nuanţă să fie reduse la o singură variantă.',
+
+	);
