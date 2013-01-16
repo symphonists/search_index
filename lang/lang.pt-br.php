6a8c9a4e223b4eb15277476d94662f4e12d18a35
Marcio Toledo <mt@marciotoledo.com>
2013-01-16 02:19:06 -0200

    Add Portuguese (Brazil) language

---
+++ lang/lang.pt-br.php
@@ -0,0 +1,177 @@
+<?php
+
+	$about = array(
+		'name' => 'Português (Brasil)',
+		'author' => array(
+			'name' => 'Marcio Toledo',
+			'email' => 'mt@marciotoledo.com',
+			'website' => 'http://marciotoledo.com'
+		),
+		'release-date' => '2013-01-16'
+	);
+
+	/**
+	 * Search Index
+	 */
+	$dictionary = array(
+
+		'Search Index' => 
+		'Search Index',
+
+		'Indexes' => 
+		'Índices',
+
+		'Synonyms' => 
+		'Sinônimos',
+
+		'Logs' => 
+		'Logs',
+
+		'Keywords' => 
+		'Palavras-chave',
+
+		'Results' => 
+		'Resultados',
+
+		'No data available.' => 
+		'Nenhum dado disponível.',
+
+		'View full search logs' => 
+		'Ver todos os registros de busca',
+
+		'Indexing page {$page} of {$total}' => 
+		'Indexando página {$page} de {$total}',
+
+		'{$total} entries' => 
+		'{$total} registros',
+
+		'{$total} entry' => 
+		'{$total} registro',
+
+		'Highest' => 
+		'Mais Alto',
+
+		'High' => 
+		'Alto',
+
+		'Medium (none)' => 
+		'Médio (Nenhum)',
+
+		'Low' => 
+		'Baixo',
+
+		'Lowest' => 
+		'Mais Baixo',
+
+		'Search Indexes' => 
+		'Índices de Busca',
+
+		'Included Fields' => 
+		'Campos inclusos',
+
+		'Only selected fields will be indexed.' => 
+		'Somente campos selecionados serão indexados.',
+
+		'Weighting' => 
+		'Ponderação',
+
+		'sections' => 
+		'seções',
+
+		'authors' => 
+		'autores',
+
+		'navigation' => 
+		'navecação',
+
+		'Index Filters' => 
+		'Filtros de Índice',
+
+		'Only entries that pass these filters will be indexed.' => 
+		'Somente registros que passarem pelos filtros serão indexados.',
+
+		'Configure how each of your sections are indexed. Choose which field text values to index, which entries to index, and the weighting of the section in search results.' => 
+		'Configure como cada uma das suas seções são indexadas. Escolha qual valores de campo de texto para indexar, quais registros para indexar, e a ponderação da seção nos resultados da busca.',
+
+		'Section' => 
+		'Seção',
+
+		'Index Size' => 
+		'Tamanho do Índice',
+
+		'None Found.' => 
+		'Nada encontrado.',
+
+		'Waiting to re-index...' => 
+		'Aguardando para re-indexar...',
+
+		'entry' => 
+		'registro',
+
+		'entries' => 
+		'registros',
+
+		'No index' => 
+		'Nenhum índice',
+
+		'Re-index Entries' => 
+		'Re-indexar Registros',
+
+		'Adjusted Keywords' => 
+		'Palavras-chave Ajustadas',
+
+		'Depth' => 
+		'Profundidade',
+
+		'Session ID' => 
+		'ID da Seção',
+
+		'Filter searches containing the keywords %s' => 
+		'Filtros de busca contém a palavra-chave %s',
+
+		'Filter' => 
+		'Filtro',
+
+		'Clear' => 
+		'Limpar',
+
+		'Filter Logs' => 
+		'Registros de Filtro',
+
+		'Export CSV' => 
+		'Exportar CSV',
+
+		'<strong>%s</strong> unique searches from <strong>%s</strong> unique users via <strong>%s</strong> distinct search terms. Each search yielded an average of <strong>%s</strong> results.' => 
+		'<strong>%s</strong> pesquisas exclusivas de <strong>%s</strong> usuários únicos através <strong>%s</strong> termos de pesquisa distintas. Cada busca resultou em uma média de <strong>%s</strong> resultados.',
+
+		'Previous' => 
+		'Anterior',
+
+		'Next' => 
+		'Próximo',
+
+		'Replacement word' => 
+		'Palavra de substituição',
+
+		'Matching synonyms will be replaced with this word.' => 
+		'Encontrando sinônimos serão substituídos com esta palavra.',
+
+		'Word' => 
+		'Palavra',
+
+		'e.g. United Kingdom' => 
+		'ex. United Kingdom',
+
+		'These words will be replaced with the word above. Separate multiple words with commas.' => 
+		'Estas palavras serão substituídas pela palavra acima. Separe múltiplas palavras com vírgula.',
+
+		'e.g. UK, Great Britain, GB' => 
+		'ex. UK, Great Britain, GB',
+
+		'Create New Synonym' => 
+		'Criar Novo Sinônimo',
+
+		'Configure synonym expansion, so that common misspellings or variations of phrases can be normalised to a single phrase.' => 
+		'Configurar expansão de sinônimos, de modo que erros ortográficos comuns ou variações de frases possam ser normalizado em uma única frase.',
+
+	);
